4a2a5382c11deb76df549180519459459cf65cb2
jihyun <kimji97@naver.com>
2025-09-23 11:59:41 +0900

    회원가입글자수수정

--- user/forms.py
+++ user/forms.py
@@ -17,9 +17,24 @@ class LoginForm(forms.Form):
         )
     
 class SignupForm(forms.Form):
-    username = forms.CharField()
-    password1 = forms.CharField(widget = forms.PasswordInput)
-    password2 = forms.CharField(widget = forms.PasswordInput)
+    username = forms.CharField(
+        min_length=5,
+        widget = forms.TextInput(
+            attrs = {"placeholder": "사용자명 (5자리 이상)"},
+        )
+    )
+    password1 = forms.CharField(
+        min_length=8,
+        widget = forms.PasswordInput(
+            attrs={"placeholder": "비밀번호 (8자리 이상)"},
+        )
+        )
+    password2 = forms.CharField(
+        min_length=8,
+        widget = forms.PasswordInput(
+            attrs={"placeholder": "비밀번호 (8자리 이상)"},
+        )
+        )
     profile_image = forms.ImageField()
     short_description = forms.CharField()
 
