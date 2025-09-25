efb047561ba9b8f30c6963ce124bc8a400dc8230
jihyun <kimji97@naver.com>
2025-09-25 10:24:47 +0900

    리스트에서 검색기능 구현 및 디테일 화면 nav바 (찜목록,My페이지,Review)추가

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.db.models.query_utils import Q
 from post.models import Post 
-
+from django.urls import reverse
 # Create your views here.
     
 def ott_choice(request):
@@ -13,12 +14,21 @@ def ott_view(request, ott):
     if not request.user.is_authenticated: # 유저의 접근이 올바르지 않다면 로그인 화면으로 보내버리기
         return redirect("user:login")
 
-    posts = Post.objects.filter(content_ott__contains=ott)
+    print("ott_view 실행!")
+    
+    query = request.GET.get("q")
+    print("query:", query)
+
+    if query:
+        search = Post.objects.filter(content_name__contains = query, content_ott__contains=ott)
+    else:
+        search = Post.objects.filter(content_ott__contains=ott)
+    
     context = {
-        "posts": posts
+        "posts": search
     }
 
-    return render(request,"post/post_list.html",context)
+    return render(request,"post/post_list.html", context)
 
 def post_detail(request, id):
     if not request.user.is_authenticated: # 유저의 접근이 올바르지 않다면 로그인 화면으로 보내버리기
@@ -44,4 +54,26 @@ def post_detail(request, id):
         "post": post
     }
 
-    return render(request,"post/post_detail.html",context)
+    return render(request,"post/post_detail.html", context)
+
+def post_search(request):
+    query = request.GET.get("q")
+    print("query:", query)
+    if query:
+        search = Post.objects.filter(content_name__contains = query)
+    else:
+        search = Post.objects.all()
+    context = {"content_name": search}
+    return render(request, "post/post_list.html", context)
+
+def wish_list(request, post_id):
+    wish = Post.objects.get(id = post_id)
+    user = request.user
+
+    if user.like_posts.filter(id = wish.id).exists():
+        user.like_posts.remove(post)
+
+    else: 
+        user.like_posts.add(wish)
+
+    url_next = request.GET.get("next") or reverse("post:") + f"#post-{post.id}"
